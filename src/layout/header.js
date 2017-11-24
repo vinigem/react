928752e85bbc56c3cead67788d3c613923c120a6
Vinit Kumar <kumarv19@cat.com>
2017-11-24 16:46:01 +0530

    added styles

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <h2>My App</h2>
+        <h2><NavLink to={"/"} className="header-title">My App</NavLink></h2>
       </header>
     );
   }
